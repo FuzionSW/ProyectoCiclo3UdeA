8fe9cfdb951e45689936954efe8641ee27dc4e0c
DESKTOP-DEQFGQS\KRICKEN <maicu-dvf@hotmail.com>
2022-08-22 16:26:24 -0500

    Se ajustan los nombres de los paquetes de acuerdo con la arquitectura propuesta. Creación de Enum RoleName y las Clases Enterprise, Employee y Transaction de acuerdo con el diagrama de Entidad - Relación. Se crea el controlador projectController para el manejo de los servicios y los Get mapping de los métodos pruebaEmpresa, pruebaEmpleado y pruebaMovimiento. Se crea la interfaz EnterpriseRepository en el paquete correspondiente y el servicio EnterpriseService

---
+++ src/main/java/com/FuzionSW/UdeA/ProyectoCiclo3/Entity/Employee.java
@@ -0,0 +1,70 @@
+package com.FuzionSW.UdeA.ProyectoCiclo3.Entity;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "Employee")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+    private String email;
+    RoleName role;
+
+    @ManyToOne
+    @JoinColumn(name = "enterprise_id")
+    Enterprise enterprise;
+
+    public Employee() {
+    }
+
+    public Employee(long id, String name, String email, RoleName role, Enterprise enterprise) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.role = role;
+        this.enterprise = enterprise;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public RoleName getRole() {
+        return role;
+    }
+
+    public void setRole(RoleName role) {
+        this.role = role;
+    }
+
+    public Enterprise getEnterprise() {
+        return enterprise;
+    }
+
+    public void setEnterprise(Enterprise enterprise) {
+        this.enterprise = enterprise;
+    }
+}
---
+++ src/main/java/com/FuzionSW/UdeA/ProyectoCiclo3/Entity/RoleName.java
@@ -0,0 +1,23 @@
+package com.FuzionSW.UdeA.ProyectoCiclo3.Entity;
+
+public enum RoleName {
+    ADMIN ("Admin"),
+    OPERATOR ("Operador");
+
+    private String roleName;
+
+    RoleName() {
+    }
+
+    RoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+}
