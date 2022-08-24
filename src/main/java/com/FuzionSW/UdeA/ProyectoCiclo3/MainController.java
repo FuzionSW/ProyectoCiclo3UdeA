19e0bab6fb2ffd793eddc8f547cba5cf17d2d379
MauricioV <mvargas.rodriguez@gmail.com>
2022-08-24 07:31:18 -0500

    Se modifica el archivo pom.xml con las dependencias de Bootstrap y Thymeleaf, se crea la interfaz gráfica del index.html agregando en la carpeta templates en resources, así como los archivos de estilos css, javascript e imagenes en la carpeta static, se crea la clase MainController para visualizar el index.html, se agrega la línea spring.jpa.properties.hibernate.show_sql=show en application.properties para visualizar los cambios en la BD PostgreSQL en consola

---
+++ src/main/java/com/FuzionSW/UdeA/ProyectoCiclo3/MainController.java
@@ -0,0 +1,13 @@
+package com.FuzionSW.UdeA.ProyectoCiclo3;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController
+{
+    @GetMapping(value= "/")
+    public String showHomePage() {
+        return "index";
+    }
+}
