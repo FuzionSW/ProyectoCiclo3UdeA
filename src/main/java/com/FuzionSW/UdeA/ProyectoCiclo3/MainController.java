9f91e553e4311f217c29a762c9ba034f182743b3
MauricioV <mvargas.rodriguez@gmail.com>
2022-08-25 06:42:07 -0500

    Se modifica el MainController en el retorno a cada una de las opciones del sidebar. Las paginas de empresas.html, ingresos-y-egresos.html y usuarios.html, se organizan dentro de la carpeta pages

--- src/main/java/com/FuzionSW/UdeA/ProyectoCiclo3/MainController.java
+++ src/main/java/com/FuzionSW/UdeA/ProyectoCiclo3/MainController.java
@@ -6,8 +6,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController
 {
-    @GetMapping(value= "/")
+    @GetMapping(value = "/")
     public String showHomePage() {
         return "index";
+    }
+
+    @GetMapping(value = "/ingresos-y-egresos")
+    public String showIngresosYEgresos() {
+        return "/pages/ingresos-y-egresos";
+    }
+
+    @GetMapping(value= "/usuarios")
+    public String showUsuarios() {
+        return "/pages/usuarios";
+    }
+
+    @GetMapping(value= "/empresas")
+    public String showEmpresas() {
+        return "/pages/empresas";
     }
 }
