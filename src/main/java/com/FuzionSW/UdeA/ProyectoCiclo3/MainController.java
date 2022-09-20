7e5a018a66da1c8b51d99e440b02251fbbc04fd0
MauricioV <mvargas.rodriguez@gmail.com>
2022-09-20 12:17:41 -0500

    Se realiza la programación de un sistema de login básico con Auth0.

--- src/main/java/com/FuzionSW/UdeA/ProyectoCiclo3/MainController.java
+++ src/main/java/com/FuzionSW/UdeA/ProyectoCiclo3/MainController.java
@@ -1,13 +1,21 @@
 package com.FuzionSW.UdeA.ProyectoCiclo3;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController
 {
     @GetMapping(value = "/")
-    public String showHomePage() {
+    public String showHomePage(Model model, @AuthenticationPrincipal OidcUser principal) {
         return "index";
+    }
+
+    @GetMapping(value = "/front/main")
+    public String showMain() {
+        return "main";
     }
 }
